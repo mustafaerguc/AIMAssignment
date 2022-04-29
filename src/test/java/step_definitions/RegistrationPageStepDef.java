39cd54ed54483beb89183a749fd55a61281fc7d9
Mustafa <mustafa.erguc@hotmail.com>
2022-04-29 01:16:20 -0400

    Minor change

--- src/test/java/step_definitions/RegistrationPageStepDef.java
+++ src/test/java/step_definitions/RegistrationPageStepDef.java
@@ -2,10 +2,13 @@ package step_definitions;
 
 import Pages.HomePage;
 import Pages.RegistrationPage;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class RegistrationPageStepDef {
 
@@ -13,17 +16,17 @@ public class RegistrationPageStepDef {
     HomePage homePage;
     RegistrationPage registrationPage;
 
-//  @Given("running juiceshop application")
-//public void running_juiceshop_application() throws InterruptedException {
-//        WebDriverManager.chromedriver().setup();
-//        driver=new ChromeDriver();
-//        driver.manage().window().maximize();
-//        driver.get("http://localhost:3000");
-//      Thread.sleep(2000);
-//      homePage=new HomePage(driver);
-//      homePage.clickSchoolSign();
-//      homePage.clickAccountButton();
-//    }
+  @Given("running juiceshop application")
+public void running_juiceshop_application() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://localhost:3000");
+      Thread.sleep(2000);
+      homePage=new HomePage(driver);
+      homePage.clickSchoolSign();
+      homePage.clickAccountButton();
+    }
 
     @When("the user goes to the login page")
     public void the_user_goes_to_the_login_page() throws InterruptedException {
